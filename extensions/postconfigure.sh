a0a03edc0e9e7e03e8cebcc8aaf127e58ce18be2
David Guthrie <dguthrie@dguthrie.remote.csb>
2019-04-12 17:20:38 -0400

    Added support for variables to set the CLI values

--- extensions/postconfigure.sh
+++ extensions/postconfigure.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 echo "Executing actions.cli"
-$JBOSS_HOME/bin/jboss-cli.sh --file=$JBOSS_HOME/extensions/actions.cli
+
+# ENV needed
+# JBOSS_HOME
+# VAULT_PASS
+# VAULT_SALT
+# DB_USER
+# DB_PASSWORD
+ 
+printenv > $JBOSS_HOME/bin/env.properties
+$JBOSS_HOME/bin/jboss-cli.sh --properties=env.propertis --file=$JBOSS_HOME/extensions/actions.cli
